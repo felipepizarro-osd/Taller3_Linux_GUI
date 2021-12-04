455f55b465a1b323f3b861165714daa64436b5b3
Felipe Pizarro <felipe.pizarro02@alumnos.ucn.cl>
2021-12-03 23:43:04 -0300

    FV1

--- db1.sql
+++
@@ -1,6 +0,0 @@
-    foreign key (id_mounstruo1) references monstruos(id),
-    foreign key (id_mounstruo2) references monstruos(id),
-    foreign key (id_mounstruo3) references monstruos(id),
-    foreign key (id_mounstruo4) references monstruos(id),
-    foreign key (id_mounstruo5) references monstruos(id),
-    foreign key (id_mounstruo6) references monstruos(id),
